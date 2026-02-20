a3ffdaddf99d3c7f9cfaca008153733fde03ec4c
hrishikesh2095 <hrishikesh.freelance@gmail.com>
2026-02-20 19:39:19 +0530

    feat: create drawing on trigger of sf code

--- bartakke_erp/public/js/item.js
+++ bartakke_erp/public/js/item.js
@@ -16,6 +16,23 @@ frappe.ui.form.on('Item', {
         }
         
     },
+    custom_sf_code: function (frm) {
+        if (!frm.doc.custom_drawing_no && frm.doc.item_code && frm.doc.item_group) {
+            frappe.call({
+            method: "bartakke_erp.bartakke_erp.api.item.get_drawing",
+            args: {
+                doc: frm.doc
+            },
+            callback(r) {
+                if (r.message) {
+                    frm.set_value("custom_drawing_no", r.message);
+                }
+            }
+
+        })
+        }
+        
+    },
     custom_add_revision: function (frm) {
         console.log('ass')
         frappe.call({
