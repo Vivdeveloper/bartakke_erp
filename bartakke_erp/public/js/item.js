e18b62d94ffe201b6b4c1347feb4aa166e04796b
Vivdeveloper <50074551+Vivdeveloper@users.noreply.github.com>
2026-05-27 16:39:47 +0530

    added full code

--- bartakke_erp/public/js/item.js
+++ bartakke_erp/public/js/item.js
@@ -1,19 +1,12 @@
 frappe.ui.form.on("Item", {
     refresh(frm) {
         frm._drawing_counter_item_group = frm.doc.item_group || "";
+        frm._drawing_counter_sf_code = frm.doc.custom_sf_code || "";
     },
 
     custom_add_drawing(frm) {
         if (!frm.doc.custom_drawing_no) {
-            frappe.call({
-                method: "bartakke_erp.bartakke_erp.api.item.get_drawing",
-                args: { doc: frm.doc },
-                callback(r) {
-                    if (r.message !== undefined && r.message !== null && r.message !== "") {
-                        frm.set_value("custom_drawing_no", r.message);
-                    }
-                },
-            });
+            suggest_drawing_no(frm);
         }
     },
 
@@ -30,24 +23,36 @@ frappe.ui.form.on("Item", {
             return;
         }
 
+        frm._drawing_counter_item_group = cur;
+
         if (!cur) {
-            frm._drawing_counter_item_group = "";
             frm.set_value("custom_drawing_no", "");
             return;
         }
 
-        frappe.call({
-            method: "bartakke_erp.bartakke_erp.api.item.get_drawing",
-            args: { doc: frm.doc },
-            callback(r) {
-                if (r.message !== undefined && r.message !== null && r.message !== "") {
-                    frm.set_value("custom_drawing_no", r.message);
-                } else {
-                    frm.set_value("custom_drawing_no", "");
-                }
-                frm._drawing_counter_item_group = cur;
-            },
-        });
+        suggest_drawing_no(frm, { overwrite: true });
+    },
+
+    custom_sf_code(frm) {
+        const cur = frm.doc.custom_sf_code || "";
+
+        if (frm._drawing_counter_sf_code === undefined) {
+            frm._drawing_counter_sf_code = cur;
+            return;
+        }
+
+        const prev = frm._drawing_counter_sf_code;
+        if (cur === prev) {
+            return;
+        }
+
+        frm._drawing_counter_sf_code = cur;
+
+        if (!cur || frm.doc.custom_drawing_no) {
+            return;
+        }
+
+        suggest_drawing_no(frm);
     },
 
     after_save(frm) {
@@ -62,3 +67,28 @@ frappe.ui.form.on("Item", {
         });
     },
 });
+
+function suggest_drawing_no(frm, opts = {}) {
+    const overwrite = opts.overwrite || false;
+
+    if (!frm.doc.item_group) {
+        if (overwrite) {
+            frm.set_value("custom_drawing_no", "");
+        }
+        return;
+    }
+
+    frappe.call({
+        method: "bartakke_erp.bartakke_erp.api.item.get_drawing",
+        args: { doc: frm.doc },
+        callback(r) {
+            if (r.message !== undefined && r.message !== null && r.message !== "") {
+                if (overwrite || !frm.doc.custom_drawing_no) {
+                    frm.set_value("custom_drawing_no", r.message);
+                }
+            } else if (overwrite) {
+                frm.set_value("custom_drawing_no", "");
+            }
+        },
+    });
+}
